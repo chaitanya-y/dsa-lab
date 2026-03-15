0cb1c76744a57123194405a1f443c186c4c05519
chaitanya-y <chaitanya.yarramsetti@gmail.com>
2026-03-15 16:39:02 -0500

    Changed variable names for TwoSum

--- problemSolving/TwoSum.js
+++ problemSolving/TwoSum.js
@@ -1,27 +1,25 @@
 // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
-console.log('----Hash map approach----')
+console.log('--Optimized--Hash map approach----')
 
 function twoSum(nums, target) {
   const seen = new Map(); // number -> index
 
   for (let i = 0; i < nums.length; i++) {
-    const x = nums[i];
-    const need = target - x;
-
+    const current = nums[i];
+    const complement = target - current;
     // If we've already seen "need", we found the pair
-    if (seen.has(need)) {
-      return [seen.get(need), i];
+    if (seen.has(complement)) {
+      return [seen.get(complement), i];
     }
-
     // Otherwise remember current number for future matches
-    seen.set(x, i);
+    seen.set(current, i);
   }
 
   return "No two numbers add up to the target.";
 }
 
-//console.log('----Brute force approach----')
+// console.log('----Brute force approach----')
 // function twoSum(arr, target){
 //     for(let i=0;i<arr.length;i++){
 //         for(let j=i+1;j<arr.length;j++){
@@ -33,4 +31,4 @@ function twoSum(nums, target) {
 //     return "No two numbers add up to the target."
 // }
 
-console.log(twoSum([2,2,7,15],22))
+console.log(twoSum([2,2,7,15],9))
